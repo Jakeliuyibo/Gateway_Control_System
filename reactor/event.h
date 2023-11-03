505993d7b57d741b3adaf261f7fcf44066eecf3e
liuyibo <1299502716@qq.com>
2023-11-03 05:38:26 +0000

    编写reactor框架的event事件模块

--- reactor/event.h
+++ reactor/event.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <string>
-#include "logger.h"
 #include <boost/json.hpp>
+#include "logger.h"
 
 using namespace std;
 using namespace boost::json;
@@ -15,45 +15,31 @@ namespace reactor
     class Event
     {
         public:
-            enum EventType
-            {
-                EVENT_INIT  = 0x1,   // 事件：初始化设备
-                EVENT_READ  = 0x2,   // 事件：读取设备
-                EVENT_WRITE = 0x3,   // 事件：写入设备
-                EVENT_OTHER = 0x4,   // 其他事件
-            };
-
             // 构造
-            Event(EventType type, string dev_to)
-                :   m_type(type),
-                    m_dev_from(""),
-                    m_dev_to(dev_to),
-                    m_desc("")
-            {
-
-            }
-            Event(EventType type, string dev_from, string dev_to)
-                :   m_type(type),
-                    m_dev_from(dev_from),
-                    m_dev_to(dev_to),
-                    m_desc("")
-
-            {
-
-            }
+            Event() {}
             // 析构
-            ~Event()
-            {
-
-            }
+            ~Event() {}
+            // 添加属性
+            bool add(string key, string val);
+            // 获取值
+            bool get(string key, string &val);
         private:
             boost::json::object m_obj;
-
-            EventType m_type;
-            string m_dev_from;
-            string m_dev_to;
-            string m_desc;
     };
+
+    class DeviceEvent : Event
+    {
+        enum EventType
+        {
+            EVENT_INIT  = 0x1,   // 事件：初始化设备
+            EVENT_READ  = 0x2,   // 事件：读取设备
+            EVENT_WRITE = 0x3,   // 事件：写入设备
+            EVENT_OTHER = 0x4,   // 其他事件
+        };
+
+        
+    }
+
 
     // 序列化
     string to_str(Event &event);
