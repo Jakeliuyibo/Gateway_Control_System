e0eca7fce6873e1bf8bf39ac3422a2fbe300b1d2
zn <zhangn12581@163.com>
2023-11-16 15:13:29 +0000

    修改safequeue模块，增加阻塞非阻塞功能

--- test/test_queue.cpp
+++ test/test_queue.cpp
@@ -8,12 +8,37 @@
 #include "filetransfer.h"
 #include "event.h"
 #include "commdevice.h"
+#include "systime.h"
 
 using namespace std;
 using namespace driver;
 using namespace utility;
 using namespace reactor;
 
+
+void* Producer(void *argc)
+{  
+    SafeQueue<int> *t = (SafeQueue<int> *)argc;
+    std::cout << utility::getSystime()<<"生产者：" << "I have sleep 1s" << std::endl;
+    //先上锁然后休眠1s，形成阻塞条件
+    sleep(3);
+
+    t->enqueue(1);
+    std::cout << utility::getSystime()<<"生产者：" << "I have send data"<< 3 << std::endl;
+
+}
+void* Consumer1(void *argc)
+{
+    SafeQueue<int> *t = (SafeQueue<int> *)argc;
+    int x=99;
+    t->dequeue(x);
+    std::cout <<utility::getSystime()<< "消费者：" << "I have get data"<< x << std::endl;
+
+}
+void* Consumer2(void *argc)
+{
+
+}
 int main()
 {
     /* 初始化日志模块           */
@@ -26,15 +51,29 @@ int main()
     bool parserFlag = true;
     parserFlag = config.load("../config/defconfig.ini");
 
-    SafeQueue<int> queue;
-    queue.enqueue(1);
-    queue.enqueue(2);
+    // SafeQueue<int> queue;
+    // queue.enqueue(1);
+    // queue.enqueue(2);
     
-    int res = 0;
-    queue.dequeue(res);
-    cout << res << endl;
-    queue.dequeue(res);
-    cout << res << endl;
+    // int res = 0;
+    // queue.dequeue(res);
+    // cout << res << endl;
+    // queue.dequeue(res);
+    // cout << res << endl;
+    SafeQueue<int> queue;
+    pthread_t c1, c2, p;
+    // 创建生产者
+    pthread_create(&p, NULL,  Producer, &queue);
+
+    // 创建消费者1
+    pthread_create(&c1, NULL, Consumer1, &queue);
+
+    // 创建消费者2
+    pthread_create(&c2, NULL, Consumer2, &queue);
+
+    pthread_join(p,  NULL);
+    pthread_join(c1, NULL);
+    pthread_join(c2, NULL);
 
 
     /* 注销日志模块             */
--- utility/safequeue/safequeue.h
+++ utility/safequeue/safequeue.h
@@ -1,66 +1,72 @@
-/*
- * @Author       : liuyibo
- * @Date         : 2023-11-14 12:47:07
- * @LastEditors  : liuyibo 1299502716@qq.com
- * @LastEditTime : 2023-11-16 05:07:09
- * @FilePath     : /Gateway_Control_System/utility/safequeue/safequeue.h
- * @Description  : 模板、互斥锁、队列、阻塞非阻塞
- */
 #pragma once
 
 #include <queue>
 #include <mutex>
-
+#include <condition_variable>
 namespace utility
 {
-    template<typename T>
+    template <typename T>
     class SafeQueue
     {
-        public:
-            // 构造
-            SafeQueue() {}
-            // 析构
-            ~SafeQueue() {}
-            // 添加
-            void enqueue(T &t)
-            {
-                std::unique_lock<std::mutex> lock(m_lock);
-                m_queue.emplace(t);
-            }
-            void enqueue(T &&t)
-            {
-                std::unique_lock<std::mutex> lock(m_lock);
-                m_queue.emplace(std::move(t));
-            }
-            // 取出
-            bool dequeue(T &t)
-            {
-                std::unique_lock<std::mutex> lock(m_lock);
+    public:
+        // 构造
+        SafeQueue()
+        {
+        }
+        // 析构
+        ~SafeQueue() {}
+        // 添加
+        void enqueue(T &t)
+        {
+            std::unique_lock<std::mutex> lock(m_lock);
+            m_queue.emplace(t);
+            m_cv.notify_one();
+        }
+        void enqueue(T &&t)
+        {
+            std::unique_lock<std::mutex> lock(m_lock);
+            m_queue.emplace(std::move(t));
+            m_cv.notify_one();
+        }
 
-                /* 检测队列是否为空 */
-                if(m_queue.empty())
+        // 可选阻塞或非阻塞取出
+        bool dequeue(T &t, bool block = false)
+        {
+            std::unique_lock<std::mutex> lock(m_lock); // 添加互斥锁，若失败则堵住
+            if (m_queue.empty())
+            {
+                if (block == true) // 若需要阻塞
+                {
+                    m_cv.wait(lock, [this]
+                              { return !m_queue.empty(); });
+                }
+                else
                 {
                     return false;
                 }
+            }
+            t = std::move(m_queue.front());
+            m_queue.pop();
+            return true;
+        }
 
-                t = std::move(m_queue.front());
-                m_queue.pop();
-                return true;
-            }
-            // 是否为空
-            bool empty()
-            {
-                std::unique_lock<std::mutex> lock(m_lock);
-                return m_queue.empty();
-            }
-            // 获取队列大小
-            int size()
-            {
-                std::unique_lock<std::mutex> lock(m_lock);
-                return m_queue.size();
-            }
-        private:
-            std::mutex m_lock;
-            std::queue<T> m_queue;
+        // 是否为空
+        bool empty()
+        {
+            std::unique_lock<std::mutex> lock(m_lock);
+            return m_queue.empty();
+        }
+        // 获取队列大小
+        int size()
+        {
+            std::unique_lock<std::mutex> lock(m_lock);
+            return m_queue.size();
+        }
+
+    private:
+        std::mutex m_lock;
+        std::condition_variable m_cv;
+
+        std::queue<T> m_queue;
     };
 }
